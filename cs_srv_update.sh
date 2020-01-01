27dda75e09b7dbf41e869b4beda0f8a0cc73e590
Sven Heidemann <edraft.sh@gmail.com>
2020-01-01 16:38:28 +0100

    added update script

---
+++ cs_srv_update.sh
@@ -0,0 +1,4 @@
+bash /home/GameAdmin_EasyWI/masterserver/steamCMD/steamcmd.sh +login anonymous +force_install_dir /home/GameAdmin_EasyWI/masterserver/csgo/ +app_update 740 +quit
+
+chmod -R 777 /home/GameAdmin_EasyWI/
+chmod -R 777 /home/Gameserver/
